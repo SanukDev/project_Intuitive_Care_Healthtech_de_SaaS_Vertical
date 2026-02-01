43e53d93721e394188fd183836a375563463b4cf
Samuel Rodrigues Melo <152120362+SanukDev@users.noreply.github.com>
2026-01-31 21:36:18 -0300

    add imports to data_validation.py

--- data_validation.py
+++ data_validation.py
@@ -1,7 +1,9 @@
 from api_server import ApiCollect
 from main import chunking_func, to_zip
+import pandas as pd
 
 URL = 'https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano_de_saude_ativas/'
 
 data_validation = ApiCollect(url=URL)
 data_validation.download_file_zip()
+
