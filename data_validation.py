c1c246dccaaa020a5ea3cab4572f1c4a8f085c25
Samuel Rodrigues Melo <152120362+SanukDev@users.noreply.github.com>
2026-02-02 02:06:25 -0300

    sorting and calculating values

--- data_validation.py
+++ data_validation.py
@@ -1,6 +1,7 @@
 from api_server import ApiCollect
 from data_process import DataProcess
 import pandas as pd
+import matplotlib.pyplot as plt
 
 URL = 'https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano_de_saude_ativas/'
 
@@ -46,21 +47,45 @@ print(df_final.info())
 print(df_final.isna().sum())
 
 #-------------------- Clean duplicated data in the Dataframe
-df_final = df_final.drop(df_final[df_final['CNPJ'].duplicated()].index)
-print(df_final['VL_SALDO_INICIAL'].head(50))
+#df_final = df_final.drop(df_final[df_final['CNPJ'].duplicated()].index)
+print("\n\nvalores duplicados")
+print(df_final[['DATA','CNPJ', 'Razao_Social','VL_SALDO_INICIAL', 'VL_SALDO_FINAL','Modalidade','Nome_Fantasia']].duplicated().value_counts())
 print(df_final.shape)
+df_final = df_final.drop(df_final[df_final[['DATA','CNPJ', 'Razao_Social','VL_SALDO_INICIAL', 'VL_SALDO_FINAL','Modalidade','Nome_Fantasia']].duplicated()].index)
+print('\n\nShape after drop duplicated values')
+print(df_final.shape)
+
+# -------------
 df_final['VL_SALDO_INICIAL'] = df_final['VL_SALDO_INICIAL'].str.replace(',', '.').astype(float)
 df_final['VL_SALDO_FINAL'] = df_final['VL_SALDO_FINAL'].str.replace(',', '.').astype(float)
 print(df_final['VL_SALDO_INICIAL'].head(50))
+
+
 # Creating new final dataframe with values correct
 
 df_despesas = pd.DataFrame()
 df_despesas['CNPJ'] = df_final['CNPJ']
 df_despesas['RazaoSocial'] = df_final['Razao_Social']
 df_despesas['Trimestre'] = df_final['DATA']
-df_despesas['Ano']  = df_final['Data_Registro_ANS']
-df_despesas['ValorDespesas']  = df_final['VL_SALDO_INICIAL'] - df_final['VL_SALDO_FINAL']
-df_despesas['RegistroANS'] =  df_final['REG_ANS']
-df_despesas['Modalidade'] =  df_final['Modalidade']
+df_despesas['Ano'] = df_final['Data_Registro_ANS']
+df_despesas['ValorDespesas'] = df_final['VL_SALDO_FINAL'] - df_final['VL_SALDO_INICIAL']
+df_despesas['RegistroANS'] = df_final['REG_ANS']
+df_despesas['Modalidade'] = df_final['Modalidade']
 df_despesas['UF'] = df_final['UF']
-print(df_despesas.head())
+print(df_despesas[['RazaoSocial', 'ValorDespesas']].head())
+
+
+# Sort values by 'RazaoSocial' and 'UF
+df_despesas.sort_values(['RazaoSocial', 'UF'], ascending=True, inplace=True)
+# print(df_despesas.groupby(['RazaoSocial','UF'], as_index=False))
+print(df_despesas[['RazaoSocial', 'UF']].head(50))
+
+
+#-------------------------------Calculating values
+
+# Calc ValorDespesas by UF
+print(df_despesas[['ValorDespesas','UF']].groupby('UF').sum(numeric_only=True))
+# Calc ValorDespesas by Trimestre
+print(df_despesas[['ValorDespesas','Trimestre']].groupby('Trimestre').sum())
+
+
